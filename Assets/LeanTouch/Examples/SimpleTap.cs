a2a519eeb9f8e8adad1a7c9bd1594da99568fd81
Damien Gounot <gounot@et.esiea.fr>
2020-03-01 17:10:40 +0100

    Add Explosions

--- Assets/LeanTouch/Examples/SimpleTap.cs
+++ Assets/LeanTouch/Examples/SimpleTap.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 public class SimpleTap : MonoBehaviour
 {
 	public GameObject Prefab;
-	
-	protected virtual void OnEnable()
+    protected virtual void OnEnable()
 	{
 		// Hook into the OnFingerTap event
 		Lean.LeanTouch.OnFingerTap += OnFingerTap;
@@ -19,20 +18,24 @@ public class SimpleTap : MonoBehaviour
 	
 	public void OnFingerTap(Lean.LeanFinger finger)
 	{
+         
+
 		// Does the prefab exist?
 		if (Prefab != null)
 		{
 			// Make sure the finger isn't over any GUI elements
 			if (finger.IsOverGui == false)
 			{
-				// Clone the prefab, and place it where the finger was tapped
-				var position = finger.GetWorldPosition(50.0f);
-				var rotation = Quaternion.identity;
-				var clone    = (GameObject)Instantiate(Prefab, position, rotation);
-				
-				// Make sure the prefab gets destroyed after some time
-				Destroy(clone, 2.0f);
-			}
+
+                // Clone the prefab, and place it where the finger was tapped
+                   var position = finger.GetWorldPosition(50.0f);
+                   var rotation = Quaternion.identity;
+                   var clone = (GameObject)Instantiate(Prefab, position, rotation);
+                // Make sure the prefab gets destroyed after some time
+                  Destroy(clone, 2.0f);
+
+
+            }
 		}
 	}
 }
